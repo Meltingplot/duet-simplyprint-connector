cc4e459fab14e03f46868302e08c3f67170be0cc
Tim Schneider <tim@schneider.engineering>
2026-02-11 20:15:19 +0100

    Set SIMPLYPRINT_BACKEND to production in debug script

--- debug.sh
+++ debug.sh
@@ -1,1 +1,1 @@
-DEV_MODE=1 DEBUG=true python -m meltingplot.duet_simplyprint_connector.__main__ start
+SIMPLYPRINT_BACKEND=production DEV_MODE=1 DEBUG=true python -m meltingplot.duet_simplyprint_connector.__main__ start
